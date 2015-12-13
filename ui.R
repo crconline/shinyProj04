ebdc93eda528d19328ba1709e458f75dc3134058
crconline <crconline@gmail.com>
2015-12-13 13:29:52 -0600

    edition by myself

--- ui.R
+++ ui.R
@@ -3,7 +3,8 @@ library(shiny)
 shinyUI(pageWithSidebar(
     headerPanel("Example plot"),
     sidebarPanel(
-        sliderInput('mu', 'Guess at the mean',value = 70, min = 62, max = 74, step = 0.05)
+        sliderInput('mu', 'Guess at the mean',value = 70, min = 62, max = 74, step = 0.05),
+    print("The shorter the MSE, the closer your guess")
     ),
     mainPanel(
         plotOutput('newHist')
